84eb5e5c4113b367c1cd8c90ffd902594893cf32
Kykeon <paulinoandresmiguel@gmail.com>
2022-04-27 17:20:47 -0400

    Add custom styling to tailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      blue: '#5175FF',
+      lightblue: '#829CFF',
+      darkgrey: '#333950',
+      black: '#25293A',
+      grey: '#747B95',
+      red: '#F05B5B',
+    },
+    fontFamily: {
+      sans: ['Kumbh Sans', 'sans-serif'],
+      extend: {},
+    },
+    plugins: [],
   },
-  plugins: [],
 };
